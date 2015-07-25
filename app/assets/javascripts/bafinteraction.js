379f8f6058f53efbd99baa7624ab15d6dc208061
LeighRoberts <daroberts@newrelic.com>
2015-07-25 13:30:48 -0700

    Update bafinteraction.js
    changed slide function for the index page, enter function for the search page

--- app/assets/javascripts/bafinteraction.js
+++ app/assets/javascripts/bafinteraction.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
     $( "#action-button" ).click(function(){
-     $(".container").slideUp("slow", function(){
+     $(".index.vertical-center").slideUp(10000, function(){
         //animation complete
      });
     });
 });
 
+$(document).ready(function(){
+    $(".form-control input-md").keypress(function(e) {
+
+         if(e.which == 13) {
+             e.preventDefault();
+             $(".index.vertical-center").slideUp(10000, function(){
+                 //animation ends
+             });
+          };
+    });
+});
 
 // Drag and drop code
 
