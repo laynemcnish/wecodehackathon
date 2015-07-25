d1d5b078a4d92670cf0d11817ed7a03863286293
Layne McNish <lmcnish14@gmail.com>
2015-07-25 12:35:57 -0700

    Added job listing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,18 @@ class HomeController < ApplicationController
   end
 
   def preferences
-    params
-    binding.pry
+    @search_term = params["search_term"]
+    redirect_to job_listings_home_path({:search_term => @search_term})
+  end
+
+  def job_listings
+    @params = params
+
+    postal_code = LocationService.new.get_location_by_ip(request).postal_code.empty? ? '97217' : LocationService.new.get_location_by_ip(request).postal_code
+    search_term = params[:search_term]
+
+    service_response = JobService.new.search_by_postal_code(postal_code, search_term)
+    @jobs = service_response.entity
   end
 
   def yelp
@@ -15,22 +25,6 @@ class HomeController < ApplicationController
     search_term = params[:search_term]
 
     service_response = YelpService.new.search_by_postal_code(postal_code, search_term)
-
-    if service_response.success
-      render json: {
-                 businesses: service_response.entity,
-                 postal_code: postal_code,
-             }
-    else
-      render json: service_response.errors, status: :not_found
-    end
-  end
-
-  def jobs
-    postal_code = LocationService.new.get_location_by_ip(request).postal_code.empty? ? '97217' : LocationService.new.get_location_by_ip(request).postal_code
-    search_term = params[:search_term]
-
-    service_response = JobService.new.search_by_postal_code(postal_code, search_term)
 
     if service_response.success
       render json: {
